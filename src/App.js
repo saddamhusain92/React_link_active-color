2abbbd799ae15e9a5f205ff3462fcbaa0b1fd080
Saddam <saddam12@gmail.com>
2021-12-16 10:48:17 +0530

    full css navlink

--- src/App.js
+++ src/App.js
@@ -6,25 +6,21 @@ import {
   Route
 } from "react-router-dom";
 import Navigationbar from './component/Navigationbar'
-
 import Home from './Pages/Home'
 import About from './Pages/About';
 import Services from './Pages/Services';
 import Contacts from './Pages/Contacts'
-
-
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Navigationbar/>
-      
-    <Switch>
+      <Switch>
    
-      <Route path="/" component={Home}/>
-      <Route path="/about" component={About} />
-      <Route path="/services" component={Services} />
-      <Route path="/contacts" component={Contacts}/>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/about" component={About} />
+      <Route exact path="/services" component={Services} />
+      <Route exact path="/contacts" component={Contacts}/>
            
     </Switch>
   </BrowserRouter>
--- src/component/Navigationbar.js
+++ src/component/Navigationbar.js
@@ -5,23 +5,33 @@ function Navigationbar({history}) {
 console.log();
 const realpath = (curr) =>{
 if(history.location.pathname===curr){
-    return "red"
+    return "#626567"
 }
 }
+const bottompath = (curr) =>{
+    if(history.location.pathname===curr){
+        return "1px solid black"
+    }
+    }
+    const colorpath = (curr) =>{
+        if(history.location.pathname===curr){
+            return "#ffff"
+        }
+        }
  
     return (
         <div>
             <nav>
-            <Link to="/" style={{backgroundColor:realpath("/")}}>
+            <Link to="/" style={{backgroundColor:realpath("/"),borderBottom:bottompath("/"),color:colorpath("/")}}>
             <label>Home</label>
             </Link>
-            <Link to="about" style={{backgroundColor:realpath("/about")}}>
+            <Link to="about" style={{backgroundColor:realpath("/about"),borderBottom:bottompath("/about"),color:colorpath("/about")}}>
             <label>About</label>
             </Link>
-            <Link to="services" style={{backgroundColor:realpath("/services")}}>
+            <Link to="services" style={{backgroundColor:realpath("/services"),borderBottom:bottompath("/services"),color:colorpath("/services")}}>
             <label>Services</label>
             </Link>
-            <Link to="contacts" style={{backgroundColor:realpath("/contacts")}}>
+            <Link to="contacts" style={{backgroundColor:realpath("/contacts"),borderBottom:bottompath("/contacts"),color:colorpath("/contacts")}}>
             <label>Contact</label>
             </Link>
             
